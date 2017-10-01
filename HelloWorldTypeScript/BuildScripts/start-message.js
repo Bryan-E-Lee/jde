795429b13e8e83dfcd1264c0138e4dd1130ea963
Bryan Lee <bryan.lee@solve100.com>
2017-09-30 19:20:49 -0500

    Initial commit.

---
+++ HelloWorldTypeScript/BuildScripts/start-message.js
@@ -0,0 +1,3 @@
+import Chalk from 'chalk';
+
+console.log(Chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
---
+++ HelloWorldTypeScript/BuildScripts/test-setup.js
@@ -0,0 +1,5 @@
+﻿//Transpile w/ babel before tests run.
+require('babel-register')();
+
+//Disable webpack-only things
+require.extensions['.css'] = function(){};
